e793fb2da7b147455dd0495b034943ec022b3a7f
Alina-ps <alinaserg2014@gmail.com>
2025-01-08 20:29:26 +0100

    basic route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header.jsx';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const Nannies = lazy(() => import('./pages/NanniesPage/NanniesPage.jsx'));
+const Favourites = lazy(() =>
+  import('./pages/FavouritesPage/FavouritesPage.jsx')
+);
+const NotFound = lazy(() => import('./pages/NotFoundPage/NotFoundPage.jsx'));
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/nannies" element={<Nannies />}></Route>
+        <Route path="/favourites" element={<Favourites />}></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      {/* <p>Nanny.Services</p>
+      <p>Home</p>
+      <p>Nannies</p>
+      <p>Login</p>
+      <p>Registration</p> */}
+    </header>
+  );
+};
+
+export default Header;
