51ac4809315b713cdaa51efcd0bb0e97405a16b1
Alina-ps <alinaserg2014@gmail.com>
2025-01-10 00:19:21 +0100

    header home page add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 import clsx from 'clsx';
+import sprite from '../../assets/icons.svg';
 
 const Header = () => {
   const buildLinkClass = ({ isActive }) => {
@@ -8,7 +9,11 @@ const Header = () => {
   };
   return (
     <div className={s.header}>
-      {/* <Link to="/" className={s.logo}></Link>
+      <Link to="/" className={s.logo}>
+        <svg className={s.iconArrow} width={164} height={28}>
+          <use href={`${sprite}#icon-NannyServices`}></use>
+        </svg>
+      </Link>
       <div className={s.nav}>
         <NavLink className={buildLinkClass} to="/">
           Home
@@ -16,14 +21,18 @@ const Header = () => {
         <NavLink className={buildLinkClass} to="/nannies">
           Nannies
         </NavLink>
-        <NavLink className={buildLinkClass} to="/features">
+        {/* <NavLink className={buildLinkClass} to="/features">
           Features
-        </NavLink>
+        </NavLink> */}
       </div>
-      <div>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div> */}
+      <div className={s.actions}>
+        <Link to="/login" className={s.actionLogin}>
+          Log In
+        </Link>
+        <Link to="/register" className={s.actionRegister}>
+          Registration
+        </Link>
+      </div>
     </div>
   );
 };
